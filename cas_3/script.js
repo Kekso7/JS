44d9df941c87471c747d64cd7ed87c7ee94186be
Kekso7 <120725089+Kekso7@users.noreply.github.com>
2023-07-31 22:48:45 +0200

    Add files via upload

---
+++ cas_3/script.js
@@ -0,0 +1,10 @@
+// document.getElementById("naslovSajta").innerHTML = "Ovo je naslov iz JS";
+// document.getElementById("naslovSajta").style.color = "blue";
+
+var naslov = document.getElementById("naslovSajta");
+
+naslov.innerHTML = "Ovo je naslov iz JS";
+naslov.style.color = "red";
+
+document.getElementsByClassName("textSajta")[0].style.color = "green";
+document.getElementsByClassName("textSajta")[1].style.color = "blue";
