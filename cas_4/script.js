0def22892942d036909936720cb5fbcfbab26acd
Kekso7 <120725089+Kekso7@users.noreply.github.com>
2023-08-01 22:48:43 +0200

    Add files via upload

---
+++ cas_4/script.js
@@ -0,0 +1,18 @@
+var naslov = "Prodaja auto delova";
+
+document.getElementById("naslov").innerHTML = naslov;
+
+document.getElementById("naslovDva").innerHTML = naslov;
+
+document.getElementById("knjige").innerHTML = "harry potter 1";
+document.getElementById("knjige").innerHTML += "harry potter 2";
+
+var ime = "Marina";
+var prezime = "Krajcik";
+var godine = "31";
+
+console.log(ime + prezime + godine);
+
+var recenica = "ja se zovem " + ime + " a prezivam se " + prezime;
+
+document.getElementById("ime").innerHTML = recenica;
---
+++ cas_4/vezba.js
@@ -0,0 +1,8 @@
+var proizvodi = ["hleb", "mleko", "jogurt"];
+
+document.getElementById("proizvodi").innerHTML =
+	"<p style= 'color:red;'>" + proizvodi[0] + "</p>";
+document.getElementById("proizvodi").innerHTML +=
+	"<p style= 'color:blue;'>" + proizvodi[0] + "</p>";
+document.getElementById("proizvodi").innerHTML +=
+	"<p style= 'color:green;'>" + proizvodi[0] + "</p>";
